33b344b8fb64e911f4c8d26d52657a146852e739
Tenzin Nyima <ntenzin492@gmail.com>
2026-03-26 16:34:30 +0530

    initial commit, setup prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,54 @@
+import { PrismaClient, Prisma } from "../app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+
+//here we are setting up the connection between our prisma and the postgres database. it handles the low-level networking stuff.
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+//this takes the connection "adaptor" and creates the prisma client which is the library that let's you query the database connected.
+const prisma = new PrismaClient({
+  adapter,
+});
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    name: "Alice",
+    email: "alice@prisma.io",
+    posts: {
+      create: [
+        {
+          title: "Join the Prisma Discord",
+          content: "https://pris.ly/discord",
+          published: true,
+        },
+        {
+          title: "Prisma on YouTube",
+          content: "https://pris.ly/youtube",
+        },
+      ],
+    },
+  },
+  {
+    name: "Bob",
+    email: "bob@prisma.io",
+    posts: {
+      create: [
+        {
+          title: "Follow Prisma on Twitter",
+          content: "https://www.twitter.com/prisma",
+          published: true,
+        },
+      ],
+    },
+  },
+];
+
+export async function main() {
+  for (const u of userData) {
+    await prisma.user.create({ data: u });
+  }
+}
+
+main();
